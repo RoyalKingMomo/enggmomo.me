16242eadb04b3f703af0f9f758c28684c8c60b88
RoyalKingMomo <malahdal@ualberta.ca>
2018-03-09 23:40:43 -0700

    fix sizing and andd transition

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
-$(function () {
-    $("html, body, *").mousewheel(function (event, delta) {
-        this.scrollLeft -= (delta * 50);
-        event.preventDefault();
-    });
+function clean(node) {
+    for (var n = 0; n < node.childNodes.length; n++) {
+        var child = node.childNodes[n];
+        if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+            node.removeChild(child);
+            n--;
+        } else if (child.nodeType === 1) {
+            clean(child);
+        }
+    }
+}
+
+$(document).ready(function () {
+    clean(document);
 });
